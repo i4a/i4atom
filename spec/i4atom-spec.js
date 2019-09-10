5c41d3cb3e7c892d39b7463d66b4679a11ad8f4d
Antonio Tapiador del Dujo <atapiador@ideas4allinnovation.com>
2019-09-10 16:29:24 +0200

    Atom package scaffold

---
+++ spec/i4atom-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import I4atom from '../lib/i4atom';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('I4atom', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('i4atom');
+  });
+
+  describe('when the i4atom:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.i4atom')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'i4atom:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.i4atom')).toExist();
+
+        let i4atomElement = workspaceElement.querySelector('.i4atom');
+        expect(i4atomElement).toExist();
+
+        let i4atomPanel = atom.workspace.panelForItem(i4atomElement);
+        expect(i4atomPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'i4atom:toggle');
+        expect(i4atomPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.i4atom')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'i4atom:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let i4atomElement = workspaceElement.querySelector('.i4atom');
+        expect(i4atomElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'i4atom:toggle');
+        expect(i4atomElement).not.toBeVisible();
+      });
+    });
+  });
+});
---
+++ spec/i4atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import I4atomView from '../lib/i4atom-view';
+
+describe('I4atomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
